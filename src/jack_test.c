bbc54563df015014d929d17fcd4edbbb870a8d2f
Ry Currier <rygc1999@gmail.com>
2025-07-19 10:59:45 -0400

    stereo output; mixing input

--- src/jack_test.c
+++ src/jack_test.c
@@ -11,9 +11,7 @@
 
 /**
  * TODO:
- * stereo output
  * handle midi messages 
- * process input samples
  * better device enumeration
  * get samplerate
  * get latency
@@ -33,7 +31,7 @@ typedef struct JackClientState
 
   jack_port_t *midi_port;
   jack_port_t *input_port;
-  jack_port_t *output_port;
+  jack_port_t *output_port[2];
 } JackClientState;
 
 global JackClientState jack_client_state;
@@ -64,33 +62,31 @@ proc B32
 jack_client_connect_ports(void)
 {
   B32 result = 1;
-  jack_client_state.midi_port   = jack_port_register(jack_client_state.client, "midi",
-						     JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
-  jack_client_state.input_port  = jack_port_register(jack_client_state.client, "input",
-						     JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-  jack_client_state.output_port = jack_port_register(jack_client_state.client, "output",
-						     JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  jack_client_state.midi_port      = jack_port_register(jack_client_state.client, "midi",
+							JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+  jack_client_state.input_port     = jack_port_register(jack_client_state.client, "input",
+							JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+  jack_client_state.output_port[0] = jack_port_register(jack_client_state.client, "outputL",
+							JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  jack_client_state.output_port[1] = jack_port_register(jack_client_state.client, "outputR",
+							JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 
-  if(jack_client_state.midi_port && jack_client_state.input_port && jack_client_state.output_port) {
+  if(jack_client_state.midi_port && jack_client_state.input_port && jack_client_state.output_port[0] && jack_client_state.output_port[1]) {
     if(jack_activate(jack_client_state.client) == 0) {
       const char **input_ports =
 	jack_get_ports(jack_client_state.client, 0, 0, JackPortIsInput | JackPortIsPhysical);
 
       // TODO: better device enumeration
       if(input_ports) {
-	if(jack_connect(jack_client_state.client, jack_port_name(jack_client_state.output_port), input_ports[0]) == 0) {
+	if(jack_connect(jack_client_state.client, jack_port_name(jack_client_state.output_port[0]), input_ports[0]) == 0 &&
+	   jack_connect(jack_client_state.client, jack_port_name(jack_client_state.output_port[1]), input_ports[1]) == 0) {
 	  fprintf(stderr, "input connected\n");
 	}
 
 	for(char **input_port = (char**)input_ports; *input_port; ++input_port) {
 	  char *port_name = *input_port;
 	  fprintf(stderr, "input port: %s\n", port_name);
-	  str8_list_push(jack_client_state.arena, &jack_client_state.input_ports, Str8Cstr(port_name));
-
-	  jack_port_t *port = jack_port_by_name(jack_client_state.client, port_name);
-	  const char *port_type = jack_port_type(port);
-	  fprintf(stderr, "  type: %s\n", port_type);
-	  // TODO: connect midi port
+	  str8_list_push(jack_client_state.arena, &jack_client_state.input_ports, Str8Cstr(port_name));	  
 	}
 
 	free(input_ports);
@@ -107,6 +103,11 @@ jack_client_connect_ports(void)
 	  char *port_name = *output_port;
 	  fprintf(stderr, "output port: %s\n", port_name);
 	  str8_list_push(jack_client_state.arena, &jack_client_state.output_ports, Str8Cstr(port_name));
+
+	  jack_port_t *port = jack_port_by_name(jack_client_state.client, port_name);
+	  const char *port_type = jack_port_type(port);
+	  fprintf(stderr, "  type: %s\n", port_type);
+	  // TODO: connect midi port
 	}
 
 	free(output_ports);
@@ -143,10 +144,10 @@ jack_process(jack_nframes_t nframes, void *arg)
   JackProcessData *data = arg;
   R32 step = TAU32 * data->freq * data->sample_period;
 
-  U8  *midi = jack_port_get_buffer(jack_client_state.midi_port,   nframes);
-  R32 *in   = jack_port_get_buffer(jack_client_state.input_port,  nframes);
-  R32 *out  = jack_port_get_buffer(jack_client_state.output_port, nframes);
-  Unused(in);
+  U8  *midi  = jack_port_get_buffer(jack_client_state.midi_port,   nframes);
+  R32 *in    = jack_port_get_buffer(jack_client_state.input_port,  nframes);
+  R32 *outL  = jack_port_get_buffer(jack_client_state.output_port[0], nframes);
+  R32 *outR  = jack_port_get_buffer(jack_client_state.output_port[1], nframes);
 
   // NOTE: handle midi events
   jack_midi_event_t midi_event;
@@ -166,7 +167,7 @@ jack_process(jack_nframes_t nframes, void *arg)
   // TODO: parse events
 
   // NOTE: output samples
-  R32 *sine_samples = arena_push_array_z(scratch.arena, R32, nframes);
+  R32 *mix_buffer = arena_push_array_z(scratch.arena, R32, nframes);
 
   for(U32 frame_index = 0; frame_index < nframes; ++frame_index) {
     data->phasor += step;
@@ -174,11 +175,13 @@ jack_process(jack_nframes_t nframes, void *arg)
       data->phasor -= TAU32;
     }
 
-    R32 sample = 0.2f*sinf(data->phasor);
-    sine_samples[frame_index] = sample;
+    R32 sine_sample = 0.1f*sinf(data->phasor);
+    mix_buffer[frame_index] += sine_sample;
+    mix_buffer[frame_index] += 0.5f*in[frame_index];
   }
 
-  CopyArray(out, sine_samples, R32, nframes);
+  CopyArray(outL, mix_buffer, R32, nframes);
+  CopyArray(outR, mix_buffer, R32, nframes);
 
   arena_release_scratch(scratch);
   return(result);
