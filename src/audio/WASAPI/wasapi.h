044310d1b6a91501d84d0121ce6808a2a8ada8bf
Ry Currier <rygc1999@gmail.com>
2025-08-28 15:46:03 -0400

    nevermind, all that codegen was not necessary

--- src/audio/WASAPI/wasapi.h
+++ src/audio/WASAPI/wasapi.h
@@ -1,4 +1,4 @@
-#include  "os/core/Windows/win32_audio_defines.h"
+/* #include  "os/core/Windows/win32_audio_defines.h" */
 
-/* #include <Audioclient.h> */
-/* #include <Audiopolicy.h> */
+#include <Audioclient.h>
+#include <Audiopolicy.h>
