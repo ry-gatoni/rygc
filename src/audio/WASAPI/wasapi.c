ec7894fd979f8bd4b3cd9ce78ccd58ab6247bad2
Ry Currier <rygc1999@gmail.com>
2025-08-29 16:51:07 -0400

    more work on wasapi (not done)

--- src/audio/WASAPI/wasapi.c
+++ src/audio/WASAPI/wasapi.c
@@ -1,6 +1,90 @@
 proc B32
+wasapi_init_port(Wasapi_PortKind kind, IMMDevice *device, Wasapi_Port *port, IAudioSessionControl **session)
+{
+  B32 result = 0;
+  HRESULT err = 0;
+
+  err = IMMDevice_Activate(device, &IID_IAudioClient, CLSCTX_ALL, 0, (void**)&port->client);
+  if(err == S_OK && port->client != 0) {
+    err = IAudioClient_GetMixFormat(port->client, &port->fmt);
+    if(err == S_OK) {
+      err = IAudioClient_Initialize(port->client, AUDCLNT_SHAREMODE_SHARED, 0, 100000, 0,
+				      port->fmt, (LPCGUID)session);
+      if(err == S_OK) {
+	err = IAudioClient_GetBufferSize(port->client, &port->buffer_count);
+	if(err == S_OK) {
+	  switch(kind) {
+	  case Wasapi_PortKind_source:
+	    {
+	      err = IAudioClient_GetService(port->client, &IID_IAudioCaptureClient,
+					    (void**)&port->source);
+	    }break;
+	  case Wasapi_PortKind_sink:
+	    {
+	      err = IAudioClient_GetService(port->client, &IID_IAudioRenderClient,
+					    (void**)&port->sink);
+	    }break;
+	  default: { result = 0; } break;
+	  }
+	  if(err == S_OK && port->service != 0) {
+	    result = 1;
+	  }
+	}
+      }
+    }
+  }
+
+  return(result);
+}
+
+// TODO: Is the idea of a "port" a worthwhile generalization for WASAPI?
+//       Source and sink are handled quite differently here...
+proc Buffer
+wasapi_port_get_buffer(Wasapi_Port *port, U32 request_frame_count, U64 *timestamp)
+{
+  HRESULT err = 0;
+
+  U8 *mem = 0;
+  U32 size = 0;
+
+  DWORD flags = 0;
+  switch(port->kind) {
+  case Wasapi_PortKind_source:
+    {
+      // TODO: do we need the buffer position?
+      U32 frames_to_read = 0;
+      err = IAudioCaptureClient_GetBuffer(port->source, &mem, &frames_to_read, &flags, 0, timestamp);
+      Unused(flags); // TODO: maybe do something with the flags if they are nonzero?
+      if(err == S_OK) {
+	size = frames_to_read * port->fmt->nBlockAlign;
+      }else {
+	mem = 0;
+	size = 0;
+      }
+    }
+  case Wasapi_PortKind_sink:
+    {
+      err = IAudioRenderClient_GetBuffer(port->sink, request_frame_count, &mem);
+      if(err == S_OK) {
+	size = request_frame_count * port->fmt->nBlockAlign;
+      }else {
+	mem = 0;
+	size = 0;
+      }
+    }
+  default: {} break;
+  }
+
+  Buffer result = {0};
+  result.size = size;
+  result.mem = mem;
+  return(result);
+}
+
+proc B32
 win32_audio_init(void)
 {
+#define Win32AudioInitCheckError() if(error != S_OK) { wasapi_state = 0; arena_release(arena); goto win32_audio_init_end; }
 #define Win32AudioInitCheckErrorPtr(ptr) if(error != S_OK || ptr == 0) { wasapi_state = 0; arena_release(arena); goto win32_audio_init_end; }
   B32 result = 0;
   HRESULT error = 0;
@@ -17,6 +101,7 @@ win32_audio_init(void)
 
     // NOTE: list endpoints
     error = IMMDeviceEnumerator_EnumAudioEndpoints(wasapi_state->device_enumerator, eCapture, DEVICE_STATEMASK_ALL, &wasapi_state->input_endpoints);
+    /* error = wasapi_state->device_enumerator->lpVtbl->EnumAudioEndpoints(wasapi_state->device_enumerator, eCapture, DEVICE_STATEMASK_ALL, &wasapi_state->input_endpoints); */
     Win32AudioInitCheckErrorPtr(wasapi_state->input_endpoints);
     error = IMMDeviceEnumerator_EnumAudioEndpoints(wasapi_state->device_enumerator, eRender, DEVICE_STATEMASK_ALL, &wasapi_state->output_endpoints);
     Win32AudioInitCheckErrorPtr(wasapi_state->output_endpoints);
@@ -30,17 +115,35 @@ win32_audio_init(void)
     // TODO: get midi device
 
     // NOTE: get audio clients from input/output devices
-    error = IMMDevice_Activate(wasapi_state->input_device, IID_IAudioClient, CLSCTX_ALL, 0,
+#if 1
+    B32 source_init_success = wasapi_init_port(Wasapi_PortKind_source, wasapi_state->input_device,
+					       &wasapi_state->source_port, &wasapi_state->session);
+    B32 sink_init_success =   wasapi_init_port(Wasapi_PortKind_sink, wasapi_state->output_device,
+					       &wasapi_state->sink_port, &wasapi_state->session);
+#else
+    error = IMMDevice_Activate(wasapi_state->input_device, &IID_IAudioClient, CLSCTX_ALL, 0,
 			       (void**)&wasapi_state->input_client);
-    Win32AudioINitCheckErrorPtr(wasapi_state->input_client);
-    error = IMMDevice_Activate(wasapi_state->input_device, IID_IAudioClient, CLSCTX_ALL, 0,
+    Win32AudioInitCheckErrorPtr(wasapi_state->input_client);
+    error = IMMDevice_Activate(wasapi_state->input_device, &IID_IAudioClient, CLSCTX_ALL, 0,
 			       (void**)&wasapi_state->output_client);
     Win32AudioInitCheckErrorPtr(wasapi_state->output_client);
 
     // NOTE: add clients to audio session
-    /* error = IAudioClient_Initialize(wasapi_state->input_client, */
-    /* 				    AUDCLNT_SHAREMODE_SHARED, */
-    /* 				    AUDCLNT_STREAMFLAGS_LOOPBACK|AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM|) */
+    U32 capture_buffer_count = 0;
+    WAVEFORMATEX *capture_fmt = 0;
+    error = IAudioClient_GetMixFormat(wasapi_state->input_client, &capture_fmt);
+    Win32AudioInitCheckError();
+    error = IAudioClient_Initialize(wasapi_state->input_client,
+				    AUDCLNT_SHAREMODE_SHARED,
+				    0,
+				    100000,
+				    0,
+				    capture_fmt,
+				    (LPCGUID)&wasapi_state->session);
+    Win32AudioInitCheckErrorPtr(wasapi_state->session);
+    error = IAudioClient_GetBufferSize(wasapi_state->input_client, &capture_buffer_count);
+    Win32AudioInitCheckError();
+#endif
     
     result = 1;
   }
--- src/audio/WASAPI/wasapi.h
+++ src/audio/WASAPI/wasapi.h
@@ -1,9 +1,83 @@
 /* #include  "os/core/Windows/win32_audio_defines.h" */
 
+#if LANGUAGE_C
+#  ifndef COBJMACROS
+#    define COBJMACROS
+#  endif
+#endif
+
 #include <combaseapi.h>
 #include <mmdeviceapi.h>
-#include <audioclient.h>
-#include <audiopolicy.h>
+#include <Audioclient.h>
+#include <Audiopolicy.h>
+
+// NOTE: wasapi helpers
+// TODO: these could be generated and put in a separate file, included here
+#if 0
+static inline HRESULT
+IMMDeviceEnumerator_EnumAudioEndpoints
+(IMMDeviceEnumerator *this_, EDataFlow dataFlow, DWORD dwStateMask, IMMDeviceCollection **ppDevices)
+{ return(this_->lpVtbl->EnumAudioEndpoints(this_, dataFlow, dwStateMask, ppDevices)); }
+
+static inline HRESULT
+IMMDeviceEnumerator_GetDefaultAudioEndpoint
+(IMMDeviceEnumerator *this_, EDataFlow dataFlow, ERole role, IMMDevice **ppEndpoint)
+{ return(this_->lpVtbl->GetDefaultAudioEndpoint(this_, dataFlow, role, ppEndpoint)); }
+
+static inline HRESULT
+IMMDevice_Activate
+(IMMDevice *this_, REFIID iid, DWORD dwClsCtx, PROPVARIANT *pActivationParams, void **ppInterface)
+{ return(this_->lpVtbl->Activate(this_, iid, dwClsCtx, pActivationParams, ppInterface)); }
+
+static inline HRESULT
+IAudioClient_Initialize
+(IAudioClient *this_, AUDCLNT_SHAREMODE ShareMode, DWORD StreamFlags, REFERENCE_TIME hnsBufferDuration, REFERENCE_TIME hnsPeriodicity, const WAVEFORMATEX *pFormat, LPCGUID AudioSessionGuid)
+{ return(this_->lpVtbl->Initialize(this_, ShareMode, StreamFlags, hnsBufferDuration, hnsPeriodicity, pFormat, AudioSessionGuid)); }
+
+static inline HRESULT
+IAudioClient_GetBufferSize
+(IAudioClient *this_, UINT32 *pNumBufferFrames)
+{ return(this_->lpVtbl->GetBufferSize(this_, pNumBufferFrames)); }
+
+static inline HRESULT
+IAudioClient_GetMixFormat
+(IAudioClient *this_, WAVEFORMATEX **ppDeviceFormat)
+{ return(this_->lpVtbl->GetMixFormat(this_, ppDeviceFormat)); }
+
+static inline HRESULT
+IAudioClient_GetService
+(IAudioClient *this_, REFIID riid, void **ppv)
+{ return(this_->lpVtbl->GetService(this_, riid, ppv)); }
+#endif
+
+// NOTE: types
+typedef enum Wasapi_PortKind
+{
+  Wasapi_PortKind_source,
+  Wasapi_PortKind_sink,
+  Wasapi_PortKind_Count,
+} Wasapi_PortKind;
+
+typedef struct Wasapi_Port
+{
+  IMMDevice *device;
+  IAudioClient *client;
+  
+  WAVEFORMATEX *fmt;
+
+  U32 buffer_count;
+
+  Wasapi_PortKind kind;
+  union
+  {
+    IAudioCaptureClient *source;
+    IAudioRenderClient  *sink;
+    void                *service;
+  };
+} Wasapi_Port;
+
+proc B32 wasapi_init_port(Wasapi_PortKind kind, IMMDevice *device, Wasapi_Port *port, IAudioSessionControl **session);
+proc Buffer wasapi_port_get_buffer(Wasapi_Port *port, U32 request_frame_count, U64 *timestamp);
 
 typedef struct Wasapi_State
 {
@@ -20,7 +94,13 @@ typedef struct Wasapi_State
   IAudioClient *input_client;
   IAudioClient *output_client;
 
+  Wasapi_Port source_port;
+  Wasapi_Port sink_port;
+  //Wasapi_Port midi_port;
+
   IAudioSessionControl *session;
+
+  U32 sample_rate;
 } Wasapi_State;
 
 global Wasapi_State *wasapi_state = 0;
