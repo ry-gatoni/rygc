dfebb9ff45ee11bfddf98d91b3e041d72abbf5f5
Ry Currier <rygc1999@gmail.com>
2026-07-23 18:02:51 -0400

    coreaudio: fix conflicting CoreFoundation bindings
    move all CoreFoundation bindings to cocoa, include in coreaudio

--- src/audio/audio.core_audio.api_defs.h
+++ src/audio/audio.core_audio.api_defs.h
@@ -1,25 +1,11 @@
+#ifndef COCOA_API_DEFS
+#  include "gfx/gfx.cocoa.api_defs.h"
+#endif
+
 // -----------------------------------------------------------------------------
 // types
 
 typedef S32 OSStatus;
-typedef unsigned char Boolean;
-
-typedef U32 FourCharCode;
-typedef FourCharCode OSType;
-
-// CoreFoundation
-
-typedef long CFIndex;
-
-typedef U32 CFStringEncoding;
-
-typedef struct CFRange
-{
-  CFIndex location;
-  CFIndex length;
-} CFRange;
-
-typedef const struct __CFString *CFStringRef;
 
 // CoreAudio
 
@@ -130,29 +116,6 @@ typedef AudioComponentInstance AudioUnit;
 
 // -----------------------------------------------------------------------------
 // constants
-
-// CoreFoundation
-
-// CFStringEncoding
-enum
-{
-  kCFStringEncodingMacRoman = 0,
-  kCFStringEncodingWindowsLatin1 = 0x0500, /* ANSI codepage 1252 */
-  kCFStringEncodingISOLatin1 = 0x0201, /* ISO 8859-1 */
-  kCFStringEncodingNextStepLatin = 0x0B01, /* NextStep encoding*/
-  kCFStringEncodingASCII = 0x0600, /* 0..127 (in creating CFString, values greater than 0x7F are treated as corresponding Unicode value) */
-  kCFStringEncodingUnicode = 0x0100, /* kTextEncodingUnicodeDefault  + kTextEncodingDefaultFormat (aka kUnicode16BitFormat) */
-  kCFStringEncodingUTF8 = 0x08000100, /* kTextEncodingUnicodeDefault + kUnicodeUTF8Format */
-  kCFStringEncodingNonLossyASCII = 0x0BFF, /* 7bit Unicode variants used by Cocoa & Java */
-
-  kCFStringEncodingUTF16 = 0x0100, /* kTextEncodingUnicodeDefault + kUnicodeUTF16Format (alias of kCFStringEncodingUnicode) */
-  kCFStringEncodingUTF16BE = 0x10000100, /* kTextEncodingUnicodeDefault + kUnicodeUTF16BEFormat */
-  kCFStringEncodingUTF16LE = 0x14000100, /* kTextEncodingUnicodeDefault + kUnicodeUTF16LEFormat */
-
-  kCFStringEncodingUTF32 = 0x0c000100, /* kTextEncodingUnicodeDefault + kUnicodeUTF32Format */
-  kCFStringEncodingUTF32BE = 0x18000100, /* kTextEncodingUnicodeDefault + kUnicodeUTF32BEFormat */
-  kCFStringEncodingUTF32LE = 0x1c000100 /* kTextEncodingUnicodeDefault + kUnicodeUTF32LEFormat */
-};
 
 // CoreAudio
 
@@ -396,23 +359,6 @@ enum
 
 // -----------------------------------------------------------------------------
 // functions
-
-// CoreFoundation
-
-proc inline CFRange
-CFRangeMake(CFIndex loc, CFIndex len)
-{
-  CFRange range = {
-    .location = loc,
-    .length = len,
-  };
-  return range;
-}
-
-extern CFIndex CFStringGetLength(CFStringRef str);
-extern CFIndex CFStringGetMaximumSizeForEncoding(CFIndex length, CFStringEncoding encoding);
-extern CFIndex CFStringGetBytes(CFStringRef str, CFRange rng, CFStringEncoding encoding, U8 loss_byte, Boolean external_representation, U8 *buf, CFIndex max_buf_len, CFIndex *used_buf_len);
-extern Boolean CFStringGetCString(CFStringRef str, char *buffer, CFIndex buffer_size, CFStringEncoding encoding);
 
 // CoreAudio
 
